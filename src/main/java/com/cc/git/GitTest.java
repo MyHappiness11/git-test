52173f317374a8daa920bab56b3787989605f06a
cys <743787408@qq.com>
2023-11-29 09:55:58 +0800

    master更新成功..............

--- src/main/java/com/cc/git/GitTest.java
+++ src/main/java/com/cc/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Git3 你好。。。");
         System.out.println("Git4 你好。。。");
         System.out.println("version 更新成功");
+        System.out.println("master更新成功..............");
     }
 }
